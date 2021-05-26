57e1cfb4e0fb32c65bdc491f0e555d24d7979e57
anura <ashrwankhede@gmail.com>
2021-05-26 23:05:35 +0530

    Completed Section 7: Javascript in the Browser

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,9 +9,44 @@ const notes = [{
     body:"Get milk, butter, bread, and eggs"
 }]
 
+const filters = {
+    searchText:''
+}
 
-const button1 = document.querySelector("button");
-button1.addEventListener('click', function(event){
-    console.log("Button Clicked");
-    console.log(event)
+// Button - Click this Button with CLICK event listner
+// const button1 = document.querySelector("button");
+// button1.addEventListener('click', function(event){
+//     console.log("Button Clicked");
+//     console.log(event)
+// })
+
+// Function to add element to a location in html
+const addElement = function (typeOfElement, elementText, elementLocation) {
+    let newElement = document.createElement(typeOfElement);
+    newElement.textContent = elementText;
+    document.querySelector(elementLocation).appendChild(newElement);
+}
+
+const renderNotes = function (array, filter){
+    const filteredNotes = array.filter(function (item) {
+        return item.title.toLowerCase().includes(filter.searchText.toLowerCase());
+    })
+    document.querySelector("#notes-display-div").innerHTML='';
+    filteredNotes.forEach(function(item){
+        addElement("p", item.title, "#notes-display-div");
+    })
+}
+renderNotes(notes, filters)
+
+// Input field with INPUT event listner
+const searchInput = document.querySelector("#seach-input-field");
+searchInput.addEventListener("input", function(event){
+    console.log(event.target.value);
+    filters.searchText = event.target.value;
+    renderNotes(notes, filters);
+})
+
+//Drop down listner
+document.querySelector("#filter-by").addEventListener("change", function(e){
+    console.log(e.target.value);
 })
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -12,8 +12,19 @@ const todos = [{
     completed:false
 },{
     title:"Order Food",
+    completed:true
+},{
+    title:"Talk to Doctor",
     completed:false
+},{
+    title:"Read Pizza Recipe",
+    completed:true
 }]
+
+const filters = {
+    title: '',
+    completed:null
+}
 
 // -------------------------------------------------------
 // DOM Challenge:
@@ -30,26 +41,87 @@ const todos = [{
 // -------------------------------------------------------
 // Adding to DOM Challenge:
 // -------------------------------------------------------
-let noOfUnfinished = 0;
-todos.forEach(function(item){
+
+// Printing unfinished todos line
+// let noOfUnfinished = 0;
+// todos.forEach(function(item){
+//     if(!item.completed){
+//         noOfUnfinished +=1;
+//     }
+// })
+// const numberOfTodosLeftMessage = document.createElement("p");
+// numberOfTodosLeftMessage.textContent=`You have ${noOfUnfinished} Unfinished Todos`;
+// document.querySelector("#remaining-todos").appendChild(numberOfTodosLeftMessage);
+
+
+// New function to create element assign text to it and then append it to a something
+const addElement = function(typeOfElement, textOfElement, locationOfAppend){
+    const element = document.createElement(typeOfElement);
+    element.textContent = textOfElement;
+    document.querySelector(locationOfAppend).appendChild(element);
+}
+
+// New Function for Rendering todos
+const renderTodos = function (arr, filters){
+    const filteredTodos = arr.filter(function(item){
+        if(filters.completed===true){
+            return (item.title.toLowerCase().includes(filters.title.toLowerCase()) && !item.completed);
+        } else {
+            return item.title.toLowerCase().includes(filters.title.toLowerCase());
+        }
+    })
+
+    // Incomplete todos message
+    let noOfUnfinished = 0;
+    filteredTodos.forEach(function(item){
     if(!item.completed){
         noOfUnfinished +=1;
     }
-})
-const numberOfTodosLeftMessage = document.createElement("p");
-numberOfTodosLeftMessage.textContent=`You have ${noOfUnfinished} Unfinished Todos`;
-document.querySelector("body").appendChild(numberOfTodosLeftMessage);
-
-const addParagraph = function(textToAdd){
-    const newParagraph = document.createElement("p");
-    newParagraph.textContent=textToAdd;
-    document.querySelector("body").appendChild(newParagraph);
+    })
+    const numberOfTodosLeftMessage = document.createElement("p");
+    numberOfTodosLeftMessage.textContent=`You have ${noOfUnfinished} Unfinished Todos`;
+    document.querySelector("#remaining-todos").innerHTML='';
+    document.querySelector("#remaining-todos").appendChild(numberOfTodosLeftMessage);
+    document.querySelector("#todo-div").innerHTML='';
+    filteredTodos.forEach(function(item){
+        addElement("p",  item.title, "#todo-div")
+    })
+    // Incomplete todos message ends
 }
 
-todos.forEach(function(item){
-    addParagraph(item.title);
+renderTodos(todos, filters);
+// Button with CLICK event listner
+// document.querySelector("#add-todo-button").addEventListener('click', function(){
+//     console.log("Button was clicked");
+// })
+
+// Input with INPUT event listner
+const inputField = document.querySelector("#input-todo");
+inputField.addEventListener('input', function(e){
+    // console.log(e.target.value);
+    filters.title=e.target.value;
+    renderTodos(todos, filters);
 })
 
-document.querySelector("#add-todo-button").addEventListener('click', function(){
-    console.log("Button was clicked");
+// Add Todo Form event
+document.querySelector("#add-todo-form").addEventListener("submit", function(e){
+    e.preventDefault();
+    let firstName = e.target.elements.firstName.value;
+    console.log(firstName);
+    todos.push({
+        title:firstName,
+        completed:false
+    })
+    // document.querySelector("#todo-div").innerHTML='';
+    // todos.forEach(function(item){
+    //     addParagraph(item.title);
+    // })
+    renderTodos(todos, filters);
+    e.target.elements.firstName.value='';
+})
+
+//checkbox event listener
+document.querySelector("#completed-status-checkbox").addEventListener("change", function(e){
+    filters.completed=e.target.checked;
+    renderTodos(todos, filters)
 })
